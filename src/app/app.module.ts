edbb74c6d40636fdbd40235cc934a3498e92d0f1
Hrishabh <hrishabhgautam480@gmail.com>
2025-03-16 16:19:02 +0530

    Components Added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AccountSettingsComponent } from './components/settings/account-settings/account-settings.component';
+import { PrivacySettingsComponent } from './components/settings/privacy-settings/privacy-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent,
+    SettingsComponent,
+    AccountSettingsComponent,
+    PrivacySettingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/settings/privacy-settings/privacy-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-settings',
+  standalone: false,
+  templateUrl: './privacy-settings.component.html',
+  styleUrl: './privacy-settings.component.css'
+})
+export class PrivacySettingsComponent {
+
+}
